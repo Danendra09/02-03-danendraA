536b41862da2f8a31221cee8459810381a0586c7
Danendra09 <danendraarshad@gmail.com>
2022-05-23 09:05:27 -0700

    Modul 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Halaman | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>Halaman About</h1>
     <h3>{{ $nama }}</h3>
     <p>{{ $email }}</p>
     <img src="image/{{ $gambar }}" alt="{{ $nama }}" width="200px">
-</body>
-</html>
+@endsection
